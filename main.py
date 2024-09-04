5cab5b8755031d56612108ba060b94ddfe6d2c1c
yildirim-murat <muratyildirim.work@gmail.com>
2024-09-04 19:01:17 +0300

    finally commit :)

---
+++ main.py
@@ -0,0 +1,57 @@
+import asyncio
+import random
+from datetime import datetime
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+app = FastAPI(title='Dummy Data Generator')
+
+
+def generate_phone_number():
+    number = "0312" + "".join([str(random.randint(0, 9)) for _ in range(7)])
+    return number
+
+
+def generate_time():
+    return datetime.now().strftime("%H:%M:%S")
+
+
+def generate_date():
+    return datetime.now().strftime("%d.%m.%Y")
+
+
+def generate_lat_long():
+    longitude = random.uniform(36.0, 42.0)
+    latitude = random.uniform(26.0, 45.0)
+    return latitude, longitude
+
+
+def generate_data():
+    phone_number = generate_phone_number()
+    time_stamp = generate_time() + " " + generate_date()
+    latitude, longitude = generate_lat_long()
+
+    data = {
+        "phone_number": phone_number,
+        "time_stamp": time_stamp,
+        "latitude": latitude,
+        "longitude": longitude
+    }
+    return data
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    print('Accepting client connection...')
+    await websocket.accept()
+    while True:
+        try:
+            delay = random.uniform(5, 10)
+            await asyncio.sleep(delay)
+            data = generate_data()
+            await websocket.send_json(data)
+            print("Data sent:", data)
+
+        except Exception as e:
+            print('WebSocket Error: ', e)
+            break
